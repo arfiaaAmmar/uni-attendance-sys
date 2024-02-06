81409eecf307c0f0fe183f59b44b54951eebcccf
arfiaa98 <hazimaammar98@gmail.com>
2024-02-06 12:04:33 +0800

    Partial CI/CD pipeline setup

--- packages/backend/src/config/config.ts
+++ packages/backend/src/config/config.ts
@@ -2,5 +2,6 @@ import express from "express";
 
 export const app = express();
 export const port = 8888;
+export const NODE_ENV = process.env.NODE_ENV
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const JWT_SECRET = process.env.JWT_SECRET;
