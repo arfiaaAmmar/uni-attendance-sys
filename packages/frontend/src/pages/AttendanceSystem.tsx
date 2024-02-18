a5a0339c7b35e4734fa57f03bbcaf5df9c63694e
Emir Haiqal <yuuzerharuto@gmail.com>
2024-02-18 17:16:56 +0800

    Added temporary fix for recent ended classes and live classes fetch

--- packages/frontend/src/pages/AttendanceSystem.tsx
+++ packages/frontend/src/pages/AttendanceSystem.tsx
@@ -2,70 +2,100 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getAllClassRecords, getLiveClassSessions, getRecentClasses } from "../api/class-record-api";
 import { ClassRecord } from "shared-library/dist/types";
-import { PAGES_PATH } from "shared-library/dist/constants";
+import { FM, PAGES_PATH } from "shared-library/dist/constants";
+import { FeedbackMessage } from "@components/shared/FeedbackMessage";
+import { formatTo12HourTime } from "@utils/constants";
 
 const AttendanceSystem = () => {
-  // const [records, setRecord] = useState<ClassRecord[]>();
-  const [recentSession, setRecentSession] = useState<ClassRecord[]>();
+  const [records, setRecords] = useState<ClassRecord[]>([])
   const [liveClasses, setLiveClasses] = useState<ClassRecord[]>([])
+  const [recentSessions, setRecentSessions] = useState<ClassRecord[]>();
+  const [success, setSuccess] = useState('');
+  const [error, setError] = useState('');
 
   async function handleDownloadPDF(_id?: string) {
-    const selectedRecord = recentSession?.find((record) => record._id === _id);
+    const selectedRecord = records?.find((record) => record._id === _id);
   };
+
+  // TODO Will use this in future
+  // useEffect(() => {
+  //   async function fetchInitialData() {
+  //     try {
+  //       setLiveClasses(await getLiveClassSessions())
+  //       setRecentSession(await getRecentClasses())
+  //     } catch (error) {
+  //       console.error("Error fetching class records:", error);
+  //     }
+  //   };
+
+  //   fetchInitialData();
+  // }, []);
 
   useEffect(() => {
     async function fetchInitialData() {
       try {
-        setLiveClasses(await getLiveClassSessions())
-        setRecentSession(await getRecentClasses())
+        setRecords(await getAllClassRecords())
+        // TODO These are just hacks. Will replace by above commented function
+        const liveClasses = records.filter(record => record.status === "Ongoing")
+        const currentTime = new Date();
+        const thirtyMinutesAgo = new Date(currentTime.getTime() - 30 * 60000)
+        const recentSessions = records.filter(record => {
+          const endTime = new Date(record?.endTime!);
+          return endTime >= thirtyMinutesAgo && endTime <= currentTime;
+        });
+        setRecentSessions(recentSessions)
+        setLiveClasses(liveClasses)
       } catch (error) {
-        console.error("Error fetching class records:", error);
+        setError(FM.errorFetchingData)
+        console.error(FM.errorFetchingData)
       }
-    };
+    }
 
-    fetchInitialData();
-  }, []);
+    fetchInitialData()
+  }, [])
 
   return (
     <>
       <h3 className="text-3xl font-bold m-4">Attendance System</h3>
       <div className="flex relative center gap-4 ml-4 mt-14 m-4">
+        <FeedbackMessage {...{ success, error }} />
         <div className="bg-neutral-300 rounded-md p-4 w-full">
           <p className="text-2xl font-bold m-2">Class Session</p>
-          <p>Shows the current running live class on your User Session</p>
+          <p className="mb-4">Shows the current running live class on your User Session</p>
           {liveClasses?.map((liveClass) => (
             <div key={liveClass?._id} className="bg-neutral-400 rounded-md p-2">
               <p className="text-2xl font-bold">{liveClass?.course}</p>
               <p className="text-lg font-bold">{liveClass?.classroom}</p>
               <div className="flex justify-between mt-8">
                 <p>Attendance: {liveClass?.attendance?.length} student(s)</p>
-                <button className="bg-red-500 px-2 py-1 text-white font-bold">
+                {/* <Link to={PAGES_PATH.classSession} className="bg-red-500 px-2 py-1 text-white font-bold">
                   End Class
-                </button>
+                </Link> */}
               </div>
             </div>
           ))}
           <Link
             to={PAGES_PATH.classSession}
-            className="bg-neutral-400 mt-2 mr-0 ml-auto block px-2 py-1 rounded-sm w-max"
+            className={`${liveClasses ? 'bg-green-500' : 'bg-neutral-400'} mt-2 mr-0 ml-auto block px-2 py-1 rounded-sm w-max`}
           >
-            Create Class
+            {liveClasses ? 'Go to Your Session' : 'Create Class'}
           </Link>
         </div>
         <div className="bg-neutral-300 rounded-md p-4 w-full">
           <p className="text-2xl font-bold m-2">Class History</p>
-          <p>Shows the current running live class on your User Session</p>
-          {recentSession?.map((prevRecord) => (
+          <p>Shows recently ended classes</p>
+          {recentSessions?.map((session) => (
             <div
-              key={prevRecord?._id}
+              key={session?._id}
               className="bg-neutral-400 rounded-md p-2 my-2"
             >
-              <p className="text-2xl font-bold">{prevRecord?.course}</p>
-              <p className="text-lg font-bold">{prevRecord?.classroom}</p>
-              <div className="flex justify-end mt-8">
+              <p className="text-2xl font-bold">{session?.course}</p>
+              <p className="text-lg font-bold">{session?.classroom}</p>
+              <p>Time Ended : {formatTo12HourTime(session?.endTime!)}</p>
+              <div className="flex mt-8 justify-end">
                 <button
                   className="bg-orange-300 px-3 py-1"
-                  onClick={() => handleDownloadPDF(prevRecord?._id)}
+                  onClick={() => handleDownloadPDF(session?._id)}
                 >
                   View | Dwnld | Print
                 </button>
